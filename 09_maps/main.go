1790e6c645ca07d4ed4abf406ab8f244c12fcd4e
Ansh Shukla <anshshuklavg>
2025-10-24 22:45:41 +0530

    maps

--- 09_maps/main.go
+++ 09_maps/main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
+
 //similar to dictionary
 func main() {
+	/*
 	m := make(map[string]string)
 	m["name"] = "golang"
 	m["area"]="backend"
 	fmt.Println(m["name"],m["area"])
+	*/
+	/*m:=make(map[string]int)
+	m["age"]=60;
+	m["price"]=45;
+	clear(m) 
+	fmt.Println(m["age"])
+	delete(m,"price") //to delete
+	//to empty whole map
+	*/
+	m:=map[string]int{"price":40,"phones":3}
+	m2:=map[string]int{"price":40,"phones":3}
+	v,ok:= m["phones"]
+	fmt.Println(v)
+	if !ok{
+		fmt.Println("all ok")
+	} else{
+		fmt.Println("not ok")
+	}
+	fmt.Println(m)
+	fmt.Println(maps.Equal(m,m2))
+
 }
